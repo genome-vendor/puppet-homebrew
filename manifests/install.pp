b533c37638427fa337e736ca1e15360675a0942f
Brian Derickson <bdericks@wustl.edu>
2016-10-17 09:03:00 -0500

    improve run time of ownership/permission check on /usr/local
    As previously written with recurse => true, Puppet would traverse
    through every file and directory under /usr/local, ensure permissions
    and ownership were correct, and generate a 'change' for every update
    that would be included in the report.
    
    This ends up being very CPU intensive and slow. It also tends to
    generate massive reports that (in my experience) can crash Puppet
    servers. So instead of using recurse => true, I've opted to use execs
    that run find, xargs, chown, chmod, etc. This is much faster, though it
    does give up visibility into exactly which files are changed and how.

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,25 +1,45 @@
 class homebrew::install {
 
-  file { ['/usr/local', '/Library/Caches/Homebrew']:
+  $dir = '/usr/local'
+  file { $dir:
     ensure  => directory,
     owner   => $homebrew::user,
     group   => $homebrew::group,
     mode    => '0775',
-    recurse => true,
+  }
+
+  exec { "chown ${dir} user ${homebrew::user}":
+    command => "chown -R ${homebrew::user} ${dir}",
+    unless  => "test $(find ${dir} -type f \\! -user ${homebrew::user} | wc -l) -eq 0",
+    path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/', '/usr/local/bin' ],
+    require => File[$dir],
+  }
+
+  exec { "chown ${dir} group ${homebrew::group}":
+    command => "chown -R :${homebrew::group} ${dir}",
+    unless  => "test $(find ${dir} -type f \\! -group ${homebrew::group} | wc -l) -eq 0",
+    path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/', '/usr/local/bin' ],
+    require => File[$dir],
+  }
+
+  exec { "chmod ${dir}":
+    command => "find ${dir} \\! -type l \\! -perm 0775 -print0 | xargs -0 chmod 0775",
+    unless  => "test $(find ${dir} \\! -type l \\! -perm 0775 | wc -l) -eq 0",
+    path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/', '/usr/local/bin' ],
+    require => File[$dir],
   }
 
   exec { 'install-homebrew':
-    cwd       => '/usr/local',
+    cwd       => $dir,
     command   => "/usr/bin/su ${homebrew::user} -c '/bin/bash -o pipefail -c \"/usr/bin/curl -skSfL https://github.com/homebrew/brew/tarball/master | /usr/bin/tar xz -m --strip 1\"'",
-    creates   => '/usr/local/bin/brew',
+    creates   => "${dir}/bin/brew",
     logoutput => on_failure,
     timeout   => 0,
-    require   => File['/usr/local'],
+    require   => File[$dir],
   } ~>
   file { '/usr/local/bin/brew':
     owner => $homebrew::user,
     group => $homebrew::group,
     mode  => '0775',
   }
-
 }
